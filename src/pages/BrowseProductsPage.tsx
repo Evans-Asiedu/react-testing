9179682ebe8daddc96b4ef292424f7ca403d4828
Evans-Asiedu <evansasiedu352@gmail.com>
2024-10-09 19:08:11 +0000

    Test for errors for browse products page

--- src/pages/BrowseProductsPage.tsx
+++ src/pages/BrowseProductsPage.tsx
@@ -56,7 +56,8 @@ function BrowseProducts() {
           <Skeleton />
         </div>
       );
-    if (errorCategories) return <div>Error: {errorCategories}</div>;
+    // if (errorCategories) return <div>Error: {errorCategories}</div>;
+    if (errorCategories) return null;
     return (
       <Select.Root
         onValueChange={(categoryId) =>
